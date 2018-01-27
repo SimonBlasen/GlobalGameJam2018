ff655b6762ba50ca732084ff08f1054460f7acd4
SimonBlasen <simon.blasen@iosb.fraunhofer.de>
2018-01-27 22:47:57 +0100

    Jumppad

---
+++ Global Game Jam 2018/Assets/Cubie/CubieJumppad.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubieJumppad : Cubie {
+
+	[SerializeField]
+	private ColliderChecker colliderPlayerChecker;
+	[SerializeField]
+	private float jumpPadStrength = 200f;
+
+	protected new void Start () {
+		base.Start();
+
+	}
+
+
+	// Update is called once per frame
+	protected new void Update () {
+		base.Update();
+
+		if (colliderPlayerChecker.colliderInside.Count > 0)
+		{
+			colliderPlayerChecker.colliderInside[0].GetComponent<Rigidbody>().AddForce(Vector3.up * jumpPadStrength);
+		}
+	}
+}
